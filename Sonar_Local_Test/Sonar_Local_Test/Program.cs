e0d130573b7519869febb9ad3216c70e3fa2924e
akp <staraneel@live.com>
2026-01-11 11:10:57 +0530

    Initial Commit

---
+++ Sonar_Local_Test/Sonar_Local_Test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sonar_Local_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
